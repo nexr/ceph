050f58417d18fd8f5b3c6702154339e4cb623d66
bgy217 <clarke.park@nexr.com>
2022-05-01 09:33:19 +0900

    move jre definition (for radosgw) to /etc/[default|sysconfing]/ceph with right option

--- etc/profile.d/ceph-radosgw.sh
+++
@@ -1,5 +0,0 @@
-if [ "x$JRE_HOME" == "x" ]; then
-  export JRE_HOME=/usr/lib/jvm/jre
-fi
-
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$JRE_HOME/lib/amd64/server:$JRE_HOME/lib/amd64:$JRE_HOME/lib:$JAVA_HOME/lib/server
