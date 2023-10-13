7c291164006dca019b10f8d9821f13d676bc42ce
bgy217 <clarke.park@nexr.com>
2023-10-13 17:18:48 +0900

    rgw_ranger_integration: fix 'does not chown for relative directory' issue

--- src/rgw/rgw_ranger_jni.cc
+++ src/rgw/rgw_ranger_jni.cc
@@ -20,7 +20,7 @@ RGWRangerJniManager::RGWRangerJniManager(CephContext* const _cct, rgw::sal::RGWR
 
   struct stat f_stat;
   if (stat(jni_config_dir.c_str(), &f_stat) != 0) {
-    if (mkdir(jni_config_dir.c_str(), 0755) == -1) {
+    if (mkdir(jni_config_dir.c_str(), 0755) != -1) {
       chown(jni_config_dir.c_str(), cct->get_set_uid(), cct->get_set_gid());
     }
     else {
