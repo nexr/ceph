c73fa4af6a9dc35eb07d824382f5d6b2016ac6b9
Clarke Geunyeong Bak <clarke.park@nexr.com>
2024-04-18 14:41:04 +0900

    [NES-55] complement PR (#54) (#55)
    * move the step removing old bucket instance to immediately after resharding and before unlocking rehard_lock
    
    * when shard info query fails, make shard query to be retried with bucket id that would have been changed by resharding
    
    * add guard_reshard to RGWRados::Bucket::UpdateIndex::complete
    
    * add guard_reshard to RGWRados::Bucket::UpdateIndex::complete_del

--- src/rgw/rgw_rados.cc
+++ src/rgw/rgw_rados.cc
@@ -6080,6 +6080,7 @@ int RGWRados::Bucket::UpdateIndex::complete(int64_t poolid, uint64_t epoch,
     return 0;
   }
   RGWRados *store = target->get_store();
+  BucketShard *bs;
 
   rgw_bucket_dir_entry ent;
   obj.key.get_index_key(&ent.key);
@@ -6105,15 +6106,14 @@ int RGWRados::Bucket::UpdateIndex::complete(int64_t poolid, uint64_t epoch,
 
   // Follows up the bucket shards that may have changed during the data write.
   // Wait for resharding that may be in progress.
-  int ret = guard_reshard(nullptr, [&](BucketShard *bs) -> int {
-           int comp_add_ret = store->cls_obj_complete_add(*bs, obj, optag, poolid, epoch, ent, category, remove_objs, bilog_flags, zones_trace);
-
-           int log_add_ret = store->svc.datalog_rados->add_entry(target->bucket_info, bs->shard_id);
-           if (log_add_ret < 0) {
-             lderr(store->ctx()) << "ERROR: failed writing data log" << dendl;
-           }
-           return comp_add_ret;
+  int ret = guard_reshard(&bs, [&](BucketShard *bs) -> int {
+           return store->cls_obj_complete_add(*bs, obj, optag, poolid, epoch, ent, category, remove_objs, bilog_flags, zones_trace);
         });
+
+  int r = store->svc.datalog_rados->add_entry(target->bucket_info, bs->shard_id);
+  if (r < 0) {
+    lderr(store->ctx()) << "ERROR: failed writing data log" << dendl;
+  }
 
   return ret;
 }
@@ -6128,13 +6128,11 @@ int RGWRados::Bucket::UpdateIndex::complete_del(int64_t poolid, uint64_t epoch,
   RGWRados *store = target->get_store();
   BucketShard *bs;
 
-  int ret = get_bucket_shard(&bs);
-  if (ret < 0) {
-    ldout(store->ctx(), 5) << "failed to get BucketShard object: ret=" << ret << dendl;
-    return ret;
-  }
-
-  ret = store->cls_obj_complete_del(*bs, optag, poolid, epoch, obj, removed_mtime, remove_objs, bilog_flags, zones_trace);
+  // Follows up the bucket shards that may have changed during the data deletion.
+  // Wait for resharding that may be in progress.
+  int ret = guard_reshard(&bs, [&](BucketShard *bs) -> int {
+           return store->cls_obj_complete_del(*bs, optag, poolid, epoch, obj, removed_mtime, remove_objs, bilog_flags, zones_trace);
+        });
 
   int r = store->svc.datalog_rados->add_entry(target->bucket_info, bs->shard_id);
   if (r < 0) {
